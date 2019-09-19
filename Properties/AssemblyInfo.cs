2b83b45b260b165791598ba374c90b307fde295c
Mans-TTCore <36418123+Mans-TTCore@users.noreply.github.com>
2019-09-19 17:30:45 -0400

    Namespace Change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wind.GH")]
+[assembly: AssemblyTitle("Aviary.Wind.GH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wind.GH")]
+[assembly: AssemblyProduct("Aviary.Wind.GH")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("919642dc-1481-416e-b86d-883a396e71be")]
+[assembly: Guid("4292aaaa-542b-41af-bdfe-9a9b498dfff8")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,16 +113,6 @@ namespace Aviary.Wind.GH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap FillPattern {
-            get {
-                object obj = ResourceManager.GetObject("FillPattern", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap FillSolid {
             get {
                 object obj = ResourceManager.GetObject("FillSolid", resourceCulture);
